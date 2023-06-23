754b9efc053cdfcfc0b428166bc36b9cba9b85b5
Francisco Emmanuel Del Moral Morales <52835031+cocisran@users.noreply.github.com>
2023-06-23 13:36:05 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
+#for ogre
+sudo apt-get install build-essential automake libtool
+sudo apt-get install libfreetype6-dev libfreeimage-dev libzzip-dev libxrandr-dev libxaw7-dev freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev
+sudo apt-get install nvidia-cg-toolkit libois-dev libboost-thread-dev
+sudo apt-get install doxygen graphviz libcppunit-dev
+####
 sudo apt install software-properties-common -y  
 sudo add-apt-repository universe -y
 
